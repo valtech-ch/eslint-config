41f0e527eb9ebd1483971dceadc29e6b9c1ce1ab
Lorenzo García Moreno <dev@lorenzogm.com>
2021-05-12 11:57:30 +0200

    split plugins and rules

--- rules/index.js
+++ rules/index.js
@@ -1,26 +1,4 @@
 module.exports = {
-  plugins: ['@typescript-eslint', 'import', 'simple-import-sort'],
-  extends: [
-    // https://www.npmjs.com/package/eslint-config-airbnb-typescript
-    'airbnb-typescript/base',
-
-    // https://www.npmjs.com/package/@typescript-eslint/eslint-plugin
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-
-    // https://www.npmjs.com/package/eslint-plugin-import
-    'plugin:import/errors',
-    'plugin:import/warnings',
-
-    /**
-     * Make sure to put prettier last, so it gets the chance to override other
-     * configs.
-     *
-     * @see https://github.com/prettier/eslint-config-prettier
-     */
-    'prettier',
-  ],
   rules: {
     // general ESLint rules
     'arrow-body-style': 'off',
--- rules/react.js
+++ rules/react.js
@@ -1,76 +1,6 @@
 module.exports = {
-  extends: [
-    // https://www.npmjs.com/package/eslint-config-airbnb-typescript
-    'airbnb-typescript',
-    'airbnb/hooks',
-
-    // https://www.npmjs.com/package/@typescript-eslint/eslint-plugin
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    // "plugin:@typescript-eslint/recommended-requiring-type-checking",
-
-    // https://www.npmjs.com/package/eslint-plugin-import
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-
-    // https://www.npmjs.com/package/eslint-plugin-cypress
-    'plugin:cypress/recommended',
-
-    // https://www.npmjs.com/package/eslint-plugin-jest
-    'plugin:jest/recommended',
-    'plugin:jest/style',
-
-    // https://github.com/testing-library/eslint-plugin-jest-dom
-    'plugin:jest-dom/recommended',
-
-    // https://www.npmjs.com/package/eslint-plugin-jsx-a11y
-    'plugin:jsx-a11y/recommended',
-
-    // https://www.npmjs.com/package/eslint-plugin-react
-    'plugin:react/recommended',
-
-    // https://github.com/testing-library/eslint-plugin-testing-library
-    'plugin:testing-library/react',
-
-    /**
-     * Make sure to put prettier last, so it gets the chance to override other
-     * configs.
-     *
-     * @see https://github.com/prettier/eslint-config-prettier
-     */
-    'prettier',
-  ],
-  plugins: [
-    '@typescript-eslint',
-    'import',
-    'cypress',
-    'jest',
-    'jest-dom',
-    'jsx-a11y',
-    'react',
-    'react-hooks',
-    'simple-import-sort',
-    'testing-library',
-  ],
+  extends: ['./index.js'],
   rules: {
-    // general ESLint rules
-    'arrow-body-style': 'off',
-    'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
-    'no-console': 'warn',
-    'no-use-before-define': 'off',
-
-    // https://www.npmjs.com/package/@typescript-eslint/eslint-plugin
-    '@typescript-eslint/no-use-before-define': 'off',
-    '@typescript-eslint/no-unsafe-assignment': 'off',
-    '@typescript-eslint/no-unsafe-call': 'off',
-    '@typescript-eslint/no-unsafe-member-access': 'off',
-    '@typescript-eslint/unbound-method': 'off',
-
-    // https://www.npmjs.com/package/eslint-plugin-import
-    'import/extensions': ['error', 'never'],
-    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-
     // https://www.npmjs.com/package/eslint-plugin-react
     'react/jsx-filename-extension': ['error', { extensions: ['.ts', '.tsx'] }],
     'react/prop-types': 'off',
@@ -79,9 +9,5 @@ module.exports = {
     // https://www.npmjs.com/package/eslint-plugin-react-hooks
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
-
-    // https://www.npmjs.com/package/eslint-plugin-simple-import-sort
-    'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error',
   },
 }
